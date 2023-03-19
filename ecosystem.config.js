c8c2d80b5bc66f463409dd7593a214f394985231
FY-SSGG <fy-ssgg@outlook.com>
2023-03-19 06:31:19 +0000

    增加环境变量

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  apps: [{
-    script: './dist/bot.js',
-    watch: './dist',
-    env: {
-      BOTTOKEN: '',//机器人Token
-      HOSTID:'',//bot所有者ID,包含数据写入权限,须特别注意.
-    }
-  }],
+    apps: [{
+        script: './dist/bot.js',
+        watch: './dist',
+        env: {
+            BOTTOKEN: '', //机器人Token
+            HOSTID: '', //bot所有者ID,包含数据写入权限,须特别注意.
+            HEFENGKEY: '', //和风天气key
+            YOUTUBERAPI: '', //YouTube Data API v3 key
+            BUSERNAME: '', //bilibili管理账户
+            BPASSWORD: '', //bilibili管理密码
+            BURL: 'http://localhost:2356', //bilibili录播机开放的本地管理地址
+        }
+    }],
 };
